a26c979546f6c74c787d6cb9c4283234e75b0b54
Vikas Kannurpatti Jayaram <vikas.ramasethu@gmail.com>
2024-03-06 15:56:17 -0500

    initial commit

---
+++ myaccount/lib/auth0.mfa.service.js
@@ -0,0 +1,58 @@
+const ManagementClient = require("auth0").ManagementClient;
+
+// Set up Auth0 configuration
+const authConfig = {
+    domain: process.env.DOMAIN,
+    audience: `${process.env.DOMAIN}/api/v2`,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+};
+
+// To obtain a Management API token from your Node backend, you can use Client Credentials Grant using your registered Auth0 Non Interactive Clients
+
+const management = new ManagementClient({
+    domain: authConfig.domain,
+    clientId: authConfig.clientId,
+    clientSecret: authConfig.clientSecret
+});
+
+async function getFactorsForTenant() {
+    try {
+        return await management.getGuardianFactors();
+    } catch (err) {
+        return err;
+    }
+ }
+async function getAuthenticatorsForUser (id) {
+    try {
+        var user = await management.users.getAuthenticationMethods({id: id});
+        return user;
+    } catch (err) {
+        return err;
+    }
+}
+
+async function deleteAuthenticatorById (id, authentication_method_id) {
+    try {
+        var response = await management.users.deleteAuthenticationMethodById({id, authentication_method_id});
+        return response
+    } catch (err) {
+        return err;
+    }
+}
+
+async function generateRecoveryCode (id, authentication_method_id) {
+    try {
+        var response = await management.users.regenerateRecoveryCode({id});
+        return response
+    } catch (err) {
+        return err;
+    }
+}
+
+module.exports = {
+    getFactorsForTenant,
+    getAuthenticatorsForUser,
+    deleteAuthenticatorById,
+    generateRecoveryCode
+}
---
+++ myaccount/routes/index.js
@@ -0,0 +1,139 @@
+var router = require('express').Router();
+var _ = require('lodash');
+const { requiresAuth } = require('express-openid-connect');
+const mfaHelper = require('../lib/auth0.mfa.service');
+const allowedMfaFactor = require('../utils/allowed-mfa-factors');
+
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'My Profile',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/profile', requiresAuth(), async function (req, res, next) {
+  try {
+    console.log(req.oidc.user?.sub);
+    const factors = await mfaHelper.getFactorsForTenant();
+    console.log(factors);
+    const authenticators = await mfaHelper.getAuthenticatorsForUser(req.oidc.user?.sub);
+    var allwedFactorList = _.merge(_.keyBy(factors, 'name'), _.keyBy(allowedMfaFactor, 'name'));
+    var factorList = _.values(allwedFactorList);
+    let merged = [];
+    if (authenticators.length > 0) {
+      for (let i = 0; i < factorList.length; i++) {
+        merged.push({
+          ...factorList[i],
+          ...authenticators.find(item => ((item.type === factorList[i].type)))
+        });
+      }
+    } else {
+      merged = factorList;
+    }
+    console.log(`Authenticators ${JSON.stringify(authenticators)}`);
+    res.render('profile', {
+      userProfile: JSON.stringify(req.oidc.user, null, 2),
+      allowedMfaFactor: merged,
+      authenticators,
+      title: 'Profile page'
+    });
+  } catch (err) {
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  }
+});
+
+router.get('/factor/add', requiresAuth(), async function (req, res) {
+  try {
+    console.log(req.oidc.user?.sub, req.params.type);
+    return res.oidc.login({
+      returnTo: '/profile',
+      authorizationParams: {
+        redirect_uri: 'http://localhost:3000/callback',
+        acr_values: "http://schemas.openid.net/pape/policies/2007/06/multi-factor",
+        factor_action: 'add'
+      },
+    });
+  } catch (err) {
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  }
+});
+
+router.get('/factor/add/:type', requiresAuth(), async function (req, res) {
+  try {
+    console.log(req.oidc.user?.sub, req.params.type);
+    return res.oidc.login({
+      returnTo: '/profile',
+      authorizationParams: {
+        redirect_uri: 'http://localhost:3000/callback',
+        acr_values: "http://schemas.openid.net/pape/policies/2007/06/multi-factor",
+        factor_type: req.params.type,
+        factor_action: 'add'
+      },
+    });
+  } catch (err) {
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  }
+});
+
+router.get('/removeFactor/:id', requiresAuth(), async function (req, res) {
+  try {
+    console.log(req.oidc.user);
+    if (req.oidc.user?.factor_id) {
+      await mfaHelper.deleteAuthenticatorById(req.oidc.user?.sub, req.params.id);
+    }
+    return res.redirect('/login');
+  } catch (error) {
+    res.render('error', {
+      message: error.message,
+      error: error
+    });
+  }
+});
+
+router.get('/factor/delete/:id', requiresAuth(), async function (req, res) {
+  try {
+    console.log(req.oidc.user?.sub, req.params.id);
+    //await mfaHelper.deleteAuthenticatorById(req.oidc.user?.sub, req.params.id);
+    //return res.redirect('/profile');
+    return res.oidc.login({
+      returnTo: `/removeFactor/${req.params.id}`,
+      authorizationParams: {
+        redirect_uri: 'http://localhost:3000/callback',
+        acr_values: "http://schemas.openid.net/pape/policies/2007/06/multi-factor",
+        factor_id: req.params.id,
+        factor_action: 'remove'
+      },
+    })
+  } catch (error) {
+    console.error(error);
+    res.render('error', {
+      message: error.message,
+      error: error
+    });
+  }
+});
+
+router.get('/factor/recovery-code', requiresAuth(), async function (req, res) {
+  try {
+    console.log(req.oidc.user?.sub);
+    const response = await mfaHelper.generateRecoveryCode(req.oidc.user?.sub);
+    console.log(response);
+    return res.json(response);
+  } catch (err) {
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  }
+});
+module.exports = router;
